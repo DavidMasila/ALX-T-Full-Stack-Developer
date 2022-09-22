a67710585df4070e0d72bcc53f5a81b68b4c77a6
MasilaDavid <davidmasila97@gmail.com>
2022-09-22 06:21:02 +0300

    Full Stack Developer

--- 2 API Development and Documentation/API testing/API testing/models.py
+++ 2 API Development and Documentation/API testing/API testing/models.py
@@ -15,7 +15,7 @@ def setup_db(app, database_path=database_path):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     # migrate.db = db
-    # migrate.app = app
+    # migrate.app =
     db.init_app(app)
     # migrate.init_app(app)
     db.create_all()
